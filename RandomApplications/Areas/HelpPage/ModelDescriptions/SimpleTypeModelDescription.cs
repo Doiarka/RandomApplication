b77066a4c49b788d563af6199e8df6da46538414
Simon <zibo99@gmail.com>
2018-11-27 10:18:03 +0500

    Добавьте файлы проекта.

---
+++ RandomApplications/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RandomApplications.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ RandomApplications/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RandomApplications.WebApiApplication" Language="C#" %>
