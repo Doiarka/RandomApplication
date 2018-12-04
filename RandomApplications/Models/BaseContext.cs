197c021d91a5d1b81570bb531b1007d83a77c809
smol <zibo99@gmail.com>
2018-12-04 21:21:19 +0500

    base context

---
+++ RandomApplications/Models/BaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace RandomApplications.Models
+{
+    public class BaseContext : DbContext
+    {
+        public BaseContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<BaseApplication> BaseApplications { get; set; }
+
+        public DbSet<BaseHistory> BaseHistories { get; set; }
+    }
+}
